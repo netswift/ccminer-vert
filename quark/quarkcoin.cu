6c83fd804cc4afe3807fedc5c6ca99493e12f33b
sp-hash <rune.stensland@bradyplc.com>
2016-04-02 18:10:24 +0200

    fixed default intensities gtx 960,970,950

--- quark/quarkcoin.cu
+++ quark/quarkcoin.cu
@@ -148,12 +148,17 @@ extern "C" int scanhash_quark(int thr_id, uint32_t *pdata,
 	cudaDeviceProp props;
 	cudaGetDeviceProperties(&props, device_map[thr_id]);
 
-	if (device_sm[device_map[thr_id]] > 500) intensity= 1 << 25;
+	if (device_sm[device_map[thr_id]] > 500) intensity= 1 << 24;
 
 	if (strstr(props.name, "980 Ti"))
 	{
 		intensity = 1 << 25;
+	} else
+	if (strstr(props.name, "980"))
+	{
+		intensity = 1 << 25;
 	}
+
 	uint32_t throughput = device_intensity(device_map[thr_id], __func__, intensity); // 256*4096
 	throughput = min(throughput, max_nonce - first_nonce);
 
